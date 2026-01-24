b810bcd39173eb3898899d8b789d72db6f381da7
Gevorg Ghazaryan <gevorg20112020@gmail.com>
2026-01-24 23:58:52 +0400

    Sign Up

---
+++ app/src/main/java/com/example/gapfix/User.java
@@ -0,0 +1,15 @@
+package com.example.gapfix;
+
+public class User {
+    public String name, email, role;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String name, String email, String role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+}
